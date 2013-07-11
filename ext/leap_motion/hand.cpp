00c5632c4d634af18dd9aabc764d6f2c0deffa5c
Aaron Patterson <aaron.patterson@gmail.com>
2013-07-11 14:13:31 -0700

    HandList#empty?

--- ext/leap_motion/hand.cpp
+++ ext/leap_motion/hand.cpp
@@ -21,8 +21,22 @@ static VALUE count(VALUE self)
   return INT2NUM(list->count());
 }
 
+static VALUE empty_p(VALUE self)
+{
+  Leap::HandList * list;
+
+  Data_Get_Struct(self, Leap::HandList, list);
+
+  if (list->isEmpty()) {
+    return Qtrue;
+  }
+
+  return Qfalse;
+}
+
 void Init_leap_hand()
 {
   cHandList = rb_define_class_under(mLeapMotion, "HandList", rb_cObject);
   rb_define_method(cHandList, "count", (ruby_method_vararg *)count, 0);
+  rb_define_method(cHandList, "empty?", (ruby_method_vararg *)empty_p, 0);
 }
